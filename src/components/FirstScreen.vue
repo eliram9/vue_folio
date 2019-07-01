32e84d3c26777a66188e4e9c3cbb7bee7cfb1697
Eliram Malachi <eliram9@gmail.com>
2019-07-01 12:04:13 -0400

    first test

---
+++ src/components/FirstScreen.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Hello, I'm Eliram</h1>
+    <h1>I'm a Fronf-End & UI/UX Developer</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FirstScreen,
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
